8ba3db5ba0c7c3d46f1114c005722d4be2708d60
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-09-25 10:03:51 +0700

    add zoom in, zoom out, and grayscale

--- gui.m
+++ gui.m
@@ -22,7 +22,7 @@ function varargout = gui(varargin)
 
 % Edit the above text to modify the response to help gui
 
-% Last Modified by GUIDE v2.5 09-Sep-2018 19:16:02
+% Last Modified by GUIDE v2.5 22-Sep-2018 16:25:32
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -85,24 +85,9 @@ try
     axes(handles.axes1);
     imshow(img);
 catch
-    f = errordlg('File doesnt match','File Eror');
+    f = errordlg('File type doesnt match','File Eror');
 end
    
-
-% --- Executes on button press in pushbutton2.
-function pushbutton2_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --- Executes on button press in pushbutton3.
-function pushbutton3_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
 
 function filename_text_Callback(hObject, eventdata, handles)
 % hObject    handle to filename_text (see GCBO)
@@ -124,3 +109,65 @@ function filename_text_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in zoomin_button.
+function zoomin_button_Callback(hObject, eventdata, handles)
+% hObject    handle to zoomin_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+image = getimage(handles.axes1);
+row = 2*size(image,1);
+column = 2*size(image,2);
+newImage = zeros(row, column, 3);
+m = 1; n = 1;
+for i = 1:size(image,1)
+    for j = 1:size(image,2)
+        newImage(m,n,:) = image(i,j,:);
+        newImage(m,n+1,:) = image(i,j,:);
+        newImage(m+1,n,:) = image(i,j,:);
+        newImage(m+1,n+1,:) = image(i,j,:);
+        n = n+2;
+    end
+    m = m+2;
+    n = 1;    
+end
+newImage = uint8(newImage);
+guidata(hObject,handles);
+figure, imshow(newImage);
+
+
+% --- Executes on button press in zoomout_button.
+function zoomout_button_Callback(hObject, eventdata, handles)
+% hObject    handle to zoomout_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+image = getimage(handles.axes1);
+newImage = zeros(round(size(image,1)/2), round(size(image,2)/2), 3);
+m = 1; n = 1;
+for i = 1:size(newImage,1)
+    for j = 1:size(newImage,2)
+        newImage(i,j,:) = image(m,n,:);
+        n = round(n+2);
+    end
+    m = round(m+2);
+    n = 1;
+end
+newImage = uint8(newImage);
+guidata(hObject,handles);
+figure, imshow(newImage);
+
+% --- Executes on button press in grayscale_button.
+function grayscale_button_Callback(hObject, eventdata, handles)
+% hObject    handle to grayscale_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+img = getimage(handles.axes1);
+R = img(:,:,1);
+G = img(:,:,2);
+B = img(:,:,3);
+gray = 0.4*R + 0.3*G + 0.3*B;
+guidata(hObject,handles);
+axes(handles.axes2);
+
+imshow(gray);
